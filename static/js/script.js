e1c34bc510cb9a75801c01a3ca6fc69599997736
Saumya <saumya.singh203@gmail.com>
2020-07-22 13:20:05 +0530

    Leaderboard added

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,37 @@
+let scores = [
+    { name: "Player 1", score: 300 },
+    { name: "Player 2", score: 370 },
+    { name: "Player 3", score: 500 },
+    { name: "Player 4", score: 430 },
+    { name: "Player 5", score: 340 },
+];
 
-function level_click(thisid)
-{
-    for (var i = 1; i <=4 ; i++) {
+
+function checkscore() {
+    let j = 0;
+    for (let i = 2; j <= scores.length; i = i + 2) {
+        document.querySelectorAll(".leader-table td")[i].innerHTML = scores[j].name;
+        document.querySelectorAll(".leader-table td")[i + 1].innerHTML = scores[j].score;
+        j = j + 1;
+    }
+}
+
+
+
+function level_click(thisid) {
+    for (var i = 1; i <= 4; i++) {
         document.querySelector("#level-" + i).classList.remove("pressed");
     }
     document.querySelector("#level-" + thisid).classList.add("pressed");
 
     console.log('/level' + thisid);
-    $.getJSON('/level' + thisid, {},{});
-    
+
+    $.getJSON('/level' + thisid, {}, {});
+
 }
 
 
-function startwith(thisid)
-{
+function startwith(thisid) {
     document.querySelector("#s1").classList.remove("pressedStart");
     document.querySelector("#s2").classList.remove("pressedStart");
     document.querySelector("#s" + thisid).classList.add("pressedStart");
@@ -21,28 +39,26 @@ function startwith(thisid)
 
 
 
-function reply_click(thisid)
-{
+function reply_click(thisid) {
     console.log(thisid);
     // cell = document.getElementById(String(thisid));
     // cell.innerHTML = "O";
 
     $.getJSON('/number' + thisid, {},
-    function (data)
-    {
-        for (var i = 1; i <= 9; i++) {
-            cell = document.getElementById(String(i));
-            cell.innerHTML = data.result[i];
-        }
+        function (data) {
+            for (var i = 1; i <= 9; i++) {
+                cell = document.getElementById(String(i));
+                cell.innerHTML = data.result[i];
+            }
 
-        console.log(data.hasWon);
-        console.log(data.winBoard);
-        if (data.hasWon == 1) {
-            for (var i = 0; i < 3; i++)
-                document.getElementById(String(data.winBoard[i])).style.color = "red";
-        }
-    });
-    
+            console.log(data.hasWon);
+            console.log(data.winBoard);
+            if (data.hasWon == 1) {
+                for (var i = 0; i < 3; i++)
+                    document.getElementById(String(data.winBoard[i])).style.color = "red";
+            }
+        });
+
 }
 
 
@@ -58,20 +74,20 @@ var btn = document.getElementById("play-button");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn.onclick = function () {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 
